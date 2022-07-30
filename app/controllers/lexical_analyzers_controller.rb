350d42f6878cb1c9c351e9df2dd55c534ae57409
NVMakarenko <taradator@ukr.net>
2022-07-30 09:06:08 +0300

    2. add num_line and index of identificator

--- app/controllers/lexical_analyzers_controller.rb
+++ app/controllers/lexical_analyzers_controller.rb
@@ -3,6 +3,9 @@ class LexicalAnalyzersController < ApplicationController
   def index
     @lexical_analyzer=LexicalAnalyzer.find(1)
     @worddiv = @lexical_analyzer.progtext.split(' ')
+    @idx_ident=0
+    @idx_math=0
+    @n_line=1
   end
 
   def update
@@ -13,10 +16,9 @@ class LexicalAnalyzersController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
   private
   def text_params
     params.require(:lexical_analyzer).permit(:progtext)
   end
-
 end
--- app/helpers/lexical_analyzers_helper.rb
+++ app/helpers/lexical_analyzers_helper.rb
@@ -1,31 +1,55 @@
 module LexicalAnalyzersHelper
 
-  def classOfChar(char)
+  def class_of_char(char)
     abc =['a','b','c','d','e','f','g','h','e','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     dig = ['0','1','2','3','4','5','6','7','8','9']
+    math = ['+','-','*','/','^','(',')']
+    log = ['>','>=','<','<=','~']
 
-    if char == '.' then 'dot'
-      elsif abc.find_all{ |elem| elem == char}.size !=0 then'Letter'
-      elsif dig.find_all{ |elem| elem == char}.size !=0 then 'Digit'
-      else 'other'
+    if char == '.' || char == ':' || char=='=' || char==';' then 'dot'
+    elsif abc.find_all{ |elem| elem == char}.size !=0 then'Letter'
+    elsif dig.find_all{ |elem| elem == char}.size !=0 then 'Digit'
+    elsif math.find_all{ |elem| elem == char}.size !=0 then 'Math'
+    elsif log.find_all{ |elem| elem == char}.size !=0 then 'Log'
+    else 'other'
     end
   end
 
-  def getId(char)
+  def get_n_line(char)
+    result=get_id(char)
+    if result=='new_line'
+      @n_line+=1
+    else @n_line
+    end
+  end
+
+  def get_idx_id(char)
+    result=get_id(char)
+    @idx_ident+=1 if (result=='identificator')
+  end
+
+  def get_idx_math(char)
+    result=get_id(char)
+    @idx_math+=1 if result=='real'||result=='integer'
+  end
+
+  def get_id(char)
     hook=Array.new
     char.split('').each do |item|
-      hook.push(classOfChar(item))
+      hook.push(class_of_char(item))
     end
-    if (hook[0]=='Letter' && hook.exclude?('other')) then getLexemType(char)
-    elsif (hook.include?('other')) then getLexemType(char)
-    elsif (hook[0]=='Digit' && hook.include?('dot')) then 'real'
-    elsif (hook.include?('Digit') && hook.exclude?('Letter')) then 'integer'
-    else t('error.103')
+    if (hook.first=='Letter'&& (hook.include?('Math') ||  hook.include?('Log'))) then t('error.102')
+    elsif (hook.first=='Letter') then get_lexem_type(char)
+    elsif (hook.first=='Digit' && hook.include?('dot')&& hook.exclude?('Letter')&& hook.exclude?('Math')&& hook.exclude?('Log')) then 'real'
+    elsif (hook.first=='Digit' && hook.exclude?('dot') && hook.exclude?('Letter')&& hook.exclude?('Math') && hook.exclude?('Log')) then 'integer'
+    elsif (hook.first=='Digit' && hook.include?('dot')&& hook.include?('Letter')) then t('error.102')
+    elsif hook.include?('dot') || hook.include?('Math') then get_lexem_type(char) #assign, order, etc
+    elsif hook.include?('other') then  t('error.103')
+    else t('error.104')
     end
-
   end
 
-  def getLexemType(lexem)
+  def get_lexem_type(lexem)
     tokenHash = {
       'keyword' => ['program', 'begin', 'end', 'let', 'for', 'to', 'do', 'if', 'goto'],
       'add_op' => ['+','-'],
@@ -33,7 +57,8 @@ module LexicalAnalyzersHelper
       "log_op" => ['<','<=','>','>=','~'],
       'boolean' => ['true','false'],
       'assign' => [':='],
-      'order_opp' => ['(', ')']
+      'order_opp' => ['(', ')'],
+      'new_line' => [';']
     }
     find_token = tokenHash.find {|key, values| values.include?(lexem)}
     if find_token !=nil
@@ -41,5 +66,4 @@ module LexicalAnalyzersHelper
     else 'identificator'
     end
   end
-
 end
